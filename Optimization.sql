ff7fdcc6e0b2db6d4ff6cbc262385555e5019791
thekrishhh <122117740+thekrishhh@users.noreply.github.com>
2026-05-04 23:05:58 +0530

    Update Optimization.sql

--- Optimization.sql
+++ Optimization.sql
@@ -1,18 +1,9 @@
----
+Query Optimization & Indexing 
 
--- Query Optimization & Indexing
+🎯 Objective:
+Improve query performance by reducing execution time using indexing.
 
----
-
--- 🎯 Objective:
--- Improve query performance by reducing execution time using indexing.
-
----
-
--- 🔍 Query Used for Optimization
-
----
-
+🔍 Query Used for Optimization :
 SELECT
   artist,
   track,
@@ -23,47 +14,29 @@ WHERE artist = 'Gorillaz'
 ORDER BY views
 LIMIT 25;
 
----
+⚠️ Before Optimization
 
--- ⚠️ Before Optimization
+Observation:
+- Query uses Sequential Scan (Seq Scan)
+- Entire table is scanned
+- Higher execution time, 3.163 ms
 
----
+⚡ Optimization Step
 
--- Observation:
--- - Query uses Sequential Scan (Seq Scan)
--- - Entire table is scanned
--- - Higher execution time
-
----
-
--- ⚡ Optimization Step
-
----
-
--- Creating index on columns used in filtering
-
+Creating index on columns used in filtering :
 CREATE INDEX artist_name ON spotify (artist)
 
----
+✅ After Optimization
 
--- ✅ After Optimization
+Observation:
+- Query uses Index Scan
+- Only relevant rows are accessed
+- Execution time is reduced from 3.163 ms to 0.647 ms
 
----
-
--- Observation:
--- - Query uses Index Scan
--- - Only relevant rows are accessed
--- - Execution time is reduced
-
----
-
--- 🧠 Notes
-
----
-
--- Indexes improve performance of SELECT queries
--- especially when using WHERE, ORDER BY, and GROUP BY
-
--- However:
--- - Too many indexes can slow down INSERT/UPDATE
--- - Indexes should be created based on query usage
+🧠 Notes :
+- Indexes improve performance of SELECT queries
+- Especially when using WHERE, ORDER BY, and GROUP BY
+  
+ However:
+- Too many indexes can slow down INSERT/UPDATE
+- Indexes should be created based on query usage
